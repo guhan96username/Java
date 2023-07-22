ccfdd0b2f85372966867dfb16ef612508151dc15
guhan96username <guhan2331@gmail.com>
2023-07-22 11:24:12 +0530

    New

---
+++ src/org/pac/GitHub.java
@@ -0,0 +1,16 @@
+package org.pac;
+
+public class GitHub {
+	
+	private void subject() {
+System.out.println("subject name=core java"); 
+	}
+	private void student() {
+System.out.println("student name=Guhan");
+	}
+	public static void main(String[] args) {
+		GitHub g = new GitHub();
+		g.subject();
+		g.student();
+	}
+}
